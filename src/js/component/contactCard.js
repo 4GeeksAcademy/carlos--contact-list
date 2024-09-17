44b34086de560bdac3c3bd1a945c393cddef8e0b
Carlos Alzate <158311146+cdalzate91@users.noreply.github.com>
2024-09-17 15:40:30 +0000

    Estilos

--- src/js/component/contactCard.js
+++ src/js/component/contactCard.js
@@ -4,17 +4,20 @@ import "../../styles/contactCard.css"
 
 const ContactCard = ({ images, name, address, phone, email }) => {
     return (
-        <div className = "container border-2 d-flex gap-3 justify-content-between">
+        <div className = "container border-2 d-flex gap-3 justify-content-between p-3">
             <div className= "imageContact">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8mcdA_uoJahxn3PQ-IC9WROV-GF2wuTl2FQ&s" alt="contact_image.jpg" />
+                {/* Modificar img */}
+                <img 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8mcdA_uoJahxn3PQ-IC9WROV-GF2wuTl2FQ&s" 
+                    alt="contact_image.jpg"/>
             </div>
             <div className= "infoContact">
-                <p className="nameContact">{name} </p>
+                <strong className="nameContact">{name}</strong>
                 <p className="addressContact">{address} </p>  
                 <p className="phoneContact">{phone}</p>
                 <p className="emailContact">{email}</p>
             </div>
-            <div className="edit">
+            <div className="edit" >
                 <spam className= "editContact">Editar</spam>
                 <spam className= "deleteContact">Borrar</spam> 
             </div>
